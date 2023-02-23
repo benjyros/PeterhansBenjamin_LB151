32c735be8968851e4befb4aa6af8883a2748ae8f
benjyros <benny.peterhans@gmail.com>
2023-02-23 01:23:53 +0100

    created react app and added firebaseconfig

---
+++ wheel-of-fortune/src/config.js
@@ -0,0 +1,13 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBh5fHofLkvJ9FSeUe6fvZ7jjnuAlFlzyw",
+    authDomain: "wheel-of-fortune-9e6f7.firebaseapp.com",
+    projectId: "wheel-of-fortune-9e6f7",
+    storageBucket: "wheel-of-fortune-9e6f7.appspot.com",
+    messagingSenderId: "36936980967",
+    appId: "1:36936980967:web:cee573278f9a68452c4542",
+    measurementId: "G-K8PVLP23CT"
+  };
+
+  export default firebaseConfig;
