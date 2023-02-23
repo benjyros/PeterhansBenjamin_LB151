0b97125a2483ee482ba31b73bcee5910cca38af0
benjyros <benny.peterhans@gmail.com>
2023-02-23 02:41:19 +0100

    added login and registration

--- wheel-of-fortune/src/index.js
+++ wheel-of-fortune/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Navbar from './Navbar';
+import Login from './Login';
+import Registration from './Registration';
 import reportWebVitals from './reportWebVitals';
 
 const navbar = ReactDOM.createRoot(document.getElementById('navbar'));
@@ -20,10 +22,18 @@ export const renderNavbar = () => {
   navbar.render(<Navbar />);
 };
 
+export const renderLogin = () => {
+  root.render(<Login />);
+};
+
+export const renderRegistration = () => {
+  root.render(<Registration />);
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
 renderNavbar();
-renderApp();
+renderLogin();
